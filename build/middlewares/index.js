655a6aab60ea6d83d7eac3fdbcfa159df894a8c3
davydocsurg <davydocsurg@gmail.com>
2023-02-18 17:20:54 +0100

    update gitignore; clear cache #1

--- build/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const ErrorHandler_1 = require("./ErrorHandler");
-Object.defineProperty(exports, "errorHandler", { enumerable: true, get: function () { return ErrorHandler_1.errorHandler; } });
